3fd20d3a35acc5197b3a2c867e0a2ebb1ee4352b
Axel Uribe <axelito.ur@gmail.com>
2020-11-09 16:38:02 -0300

    Se actulizó la Base de Datos

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class animal(models.Model):
     id = models.AutoField(primary_key = True)
     nombre = models.CharField(max_length=10, blank = False, null = False)
+
+    # De esta forma especificamos la forma en la que aparece el titulo en la Base de Datos
+    class Meta:
+        verbose_name = 'animal'
+        verbose_name_plural = 'animales'
+        ordering = ['nombre']
+
+    # De esta forma se muestra el nombre del objeto en la Base de Datos
+    def __str__(self):
+        return self.nombre
