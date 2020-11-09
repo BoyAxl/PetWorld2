2dec0553ed0c414fa1090eb839cabf1f95414f4a
Axel Uribe <axelito.ur@gmail.com>
2020-11-09 16:02:19 -0300

    Se creó la Clase ánimal dentro del modelo Producto

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class animal(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=10, blank = False, null = False)
