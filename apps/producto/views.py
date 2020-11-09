4d7fe6d92c16f8e692ac67498025ab1673b16954
Axel Uribe <axelito.ur@gmail.com>
2020-11-09 18:45:22 -0300

    Se configuraró la url producto y se creó templates

--- apps/producto/views.py
+++ apps/producto/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Home(request):
+    return render(request, 'producto/index.html') #funcion que renderiza, pinta un template con la información que deseamos
