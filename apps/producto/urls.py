380d780a6cf2eafedb319a5051d54ba3dc6bc510
Axel Uribe <axelito.ur@gmail.com>
2020-11-10 17:45:04 -0300

    Listar_Animal

--- apps/producto/urls.py
+++ apps/producto/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import crearAnimal
+from .views import crearAnimal, listarAnimal
 
 urlpatterns = [
-    path('crear_animal/',crearAnimal, name = 'crear_animal')
+    path('crear_animal/',crearAnimal, name = 'crear_animal'),
+    path('listar_animal/',listarAnimal, name = 'listar_animal')
 ]
--- apps/producto/views.py
+++ apps/producto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import AnimalForm
+from .models import Animal
 
 # Create your views here.
 def Home(request):
@@ -14,3 +15,8 @@ def crearAnimal(request):
     else:
         animal_form = AnimalForm()
     return render(request,'producto/crear_animal.html',{'animal_form':animal_form})
+
+
+def listarAnimal(request):
+    animales = Animal.objects.all()
+    return render(request,'producto/listar_animal.html',{'animales' : Animal})
